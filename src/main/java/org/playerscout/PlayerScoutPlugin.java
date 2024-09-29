0beca083a57f7d93aaf1bd4650c8be15f9eecf0b
cbgama <56281592+cbgama@users.noreply.github.com>
2024-09-29 10:43:57 -0600

    1.0

---
+++ src/main/java/org/playerscout/PlayerScoutPlugin.java
@@ -0,0 +1,4 @@
+package org.playerscout;
+
+public class PlayerScoutPlugin {
+}
--- src/test/java/org/makinstan/PluginLauncher.java
+++ src/test/java/org/makinstan/PluginLauncher.java
@@ -1,4 +1,4 @@
-package com.example;
+package org.makinstan;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
